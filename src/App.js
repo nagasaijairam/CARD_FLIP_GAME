cb6f0856c379e0997947c762ed561cb1086adaed
nagasaijairam <105200947+nagasaijairam@users.noreply.github.com>
2023-11-14 10:37:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { CardItemList } from "./components/CardItemList";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <CardItemList></CardItemList>
+    </div>
+  );
+}
+
+export default App;
